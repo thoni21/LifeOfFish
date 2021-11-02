7aa5af9a551fafacd8d85dce26b71594a7e42766
OFHansen <ohans21@student.sdu.dk>
2021-11-02 09:07:54 +0100

    Removed ItemType type from food and obstacles

--- zuul-framework/worldofzuul/Food.java
+++ zuul-framework/worldofzuul/Food.java
@@ -2,7 +2,7 @@ package worldofzuul;
 
 public class Food extends Items{
 
-    public Food(String name, int turnValue, double pollutionValue, ItemType type) {
+    public Food(String name, int turnValue, double pollutionValue) {
         super(name, turnValue, pollutionValue, ItemType.FOOD);
     }
 }
--- zuul-framework/worldofzuul/Obstacles.java
+++ zuul-framework/worldofzuul/Obstacles.java
@@ -2,7 +2,7 @@ package worldofzuul;
 
 public class Obstacles extends Items{
 
-    public Obstacles(String name, int turnValue, double pollutionValue, ItemType type) {
+    public Obstacles(String name, int turnValue, double pollutionValue) {
         super(name, turnValue, pollutionValue, ItemType.OBSTACLE);
     }
 }
